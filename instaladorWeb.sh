e27a829947936c81c6125652099b978d777963ec
JeskeTheNigg4Boy <108638153+JeskeTheNigg4Boy@users.noreply.github.com>
2023-11-11 11:35:16 -0300

    Update instaladorWeb.sh

--- instaladorWeb.sh
+++ instaladorWeb.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 clear
 echo "Creando la base de datos"
+
+MYSQL_ROOT_PASSWORD="123"  # Añade esta línea para definir la contraseña de root
+
 SQL_FILE=$(mktemp)
 cat <<EOF > "$SQL_FILE"
 CREATE USER 'AdminDB'@'localhost' IDENTIFIED BY 'password';
@@ -11,11 +14,13 @@ SOURCE /home/pablo/uexproyecto.sql;
 GRANT SELECT, SHOW VIEW, RELOAD, REPLICATION CLIENT, EVENT, TRIGGER ON *.* TO 'AdminDB'@'localhost';
 FLUSH PRIVILEGES;
 EOF
+
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" < "$SQL_FILE"
 
 rm "$SQL_FILE"
-echo "Aplicnado agustes de red"
-sudo echo "Configurcion de la red
+
+echo "Aplicando ajustes de red"
+sudo echo "# Configuracion de la red
 network:
   version: 2
   renderer: networkd
@@ -26,14 +31,15 @@ network:
       gateway4: 192.168.1.1
       nameservers:
         addresses: [8.8.8.8, 8.8.4.4]" > /etc/netplan/00-installer-config.yaml
+
 sudo netplan apply
-echo "Red Configurada"
+echo "Red configurada"
 sleep 2
 
-La pagina web
+# La página web
 echo "Preparando el sitio Web"
 sleep 2
-sudo git clone -f https://github.com/JBrusco27/ProyectoUEX2.0 /var/www/
+sudo git clone https://github.com/JBrusco27/ProyectoUEX2.0 /var/www/ProyectoUEX2.0
 sleep 2
 sudo chown -R $USER:$USER /var/www/ProyectoUEX2.0
 sudo touch /etc/apache2/sites-available/ProyectoUEX2.0.conf
@@ -45,20 +51,23 @@ echo "<VirtualHost *:80>
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
-" > /etc/apache2/sites-available/ProyectoUEX2.0.conf
+" | sudo tee /etc/apache2/sites-available/ProyectoUEX2.0.conf
+
 sudo a2ensite ProyectoUEX2.0
 sudo a2dissite 000-default
 sudo systemctl reload apache2
-sudo echo "<?php
+
+sudo bash -c "echo '<?php
 
 \$servername = \"localhost:3306\"; // Dirección del servidor de la base de datos
-\$username = \"adminDB\"; // Nombre de usuario de la base de datos
+\$username = \"AdminDB\"; // Nombre de usuario de la base de datos
 \$password = \"password\"; // Contraseña de la base de datos
 \$dbname = \"uexproyecto\"; // Nombre de la base de datos
-echo "Adaptando Base De Datos"
+
 \$conn = new PDO(\"mysql:host=\$servername;dbname=\$dbname\", \$username, \$password);
 \$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>" > /var/www/ProyectoUEX2.0/Backend/connect.php
+?>' > /var/www/ProyectoUEX2.0/Backend/connect.php"
+
 sleep 1
-echo "Pagina Web en linea"
+echo "Página web en línea"
 sleep 2
