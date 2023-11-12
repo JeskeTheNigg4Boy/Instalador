82aa12c1740263c3a76dbd26ae84734310534868
JeskeTheNigg4Boy <108638153+JeskeTheNigg4Boy@users.noreply.github.com>
2023-11-12 13:43:57 -0300

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -175,7 +175,7 @@ echo "Preparando el sitio Web"
 sudo ufw allow in "Apache"
 sleep 2
 sudo rm -r /var/www/html/
-sudo git clone https://github.com/JBrusco27/ProyectoUEX2.0 /var/www/html/ProyectoUEX2.0
+sudo git clone https://github.com/JBrusco27/ProyectoUEX2.0 /var/www/ProyectoUEX2.0
 sudo mv /var/www/ProyectoUEX2.0 /var/www/html
 sleep 2
 sudo chown -R $USER:$USER /var/www/html
