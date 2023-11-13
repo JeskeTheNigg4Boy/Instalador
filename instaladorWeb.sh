53f87a1c2dfef1a7b3daf41b537a36a197c9b87e
JeskeTheNigg4Boy <108638153+JeskeTheNigg4Boy@users.noreply.github.com>
2023-11-13 20:52:23 -0300

    Update instaladorWeb.sh

--- instaladorWeb.sh
+++ instaladorWeb.sh
@@ -4,3 +4,12 @@ sudo git clone https://github.com/JBrusco27/ProyectoUEX2.0 /var/www/ProyectoUEX2
 sudo mv /var/www/ProyectoUEX2.0 /var/www/html
 sleep 2
 sudo chown -R $USER:$USER /var/www/html
+sudo echo "<?php
+
+\$servername = \"localhost:3306\"; // Dirección del servidor de la base de datos
+\$username = \"AdminDB\"; // Nombre de usuario de la base de datos
+\$password = \"password\"; // Contraseña de la base de datos
+\$dbname = \"uexproyecto\"; // Nombre de la base de datos
+\$conn = new PDO(\"mysql:host=\$servername;dbname=\$dbname\", \$username, \$password);
+\$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>" > /var/www/html/Backend/connect.php
